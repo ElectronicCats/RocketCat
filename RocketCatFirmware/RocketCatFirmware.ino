e6d8089d059f8fb5df33686bab7e0f12a99ae7d3
Alex <isseihyudoudxd@gmail.com>
2017-12-19 15:04:48 -0600

    Incluido el web y los datos.
    Falta mostrar datos en web.

--- RocketCatFirmware/RocketCatFirmware.ino
+++ RocketCatFirmware/RocketCatFirmware.ino
@@ -4,6 +4,7 @@
 #include <Adafruit_HMC5883_U.h>
 #include "I2Cdev.h"
 #include "MPU6050.h"
+#include <ESP8266WiFi.h>
 
 Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
 
@@ -31,7 +32,22 @@ MPU6050 accelgyro;
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 
+
+//////////////////////
+// WiFi Definitions //
+//////////////////////
+const char WiFiAPPSK[] = "sparkfun";
+
+WiFiServer server(80);
+
 void setup() {
+  
+{
+  setupWiFi();
+  server.begin();
+}
+
+
   Serial.begin(9600);
   Serial.println("RocketCat Iniciando"); Serial.println("");
 
@@ -172,4 +188,87 @@ void loop() {
 
   delay(500);
 
+
+//////////////////wificlient////////////
+    {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  Serial.println(req);
+  client.flush();
+
+  // Match the request
+  int val = -1; // We'll use 'val' to keep track of both the
+                // request type (read/set) and value if set. 
+  if (req.indexOf("/read") != -1)
+     val = -2; 
+  // Otherwise request will be invalid. We'll say as much in HTML
+
+  // Set GPIO5 according to the request
+  //if (val >= 0)
+    //digitalWrite(val);
+
+  client.flush();
+
+  // Prepare the response. Start with the common header:
+  String s = "HTTP/1.1 200 OK\r\n";
+  s += "Content-Type: text/html\r\n\r\n";
+  s += "<!DOCTYPE HTML>\r\n<html>\r\n";
+  
+  // If we're setting the LED, print out a message saying we did
+  //if (val >= 0)
+
+  //else//
+  if (val == -2)
+  { // If we're reading pins, print out those values:
+    s += "Pressure, Temperature and Altitude: ";
+    s += (event.pressure);
+    s += "<br>"; // Go to the next line.
+    s += "AccelGyroMetter: ";
+    s += String();
+    s += "<br>";
+    s += "MagnetoMetter: ";
+    s += String();
+  }
+  else
+  {
+    s += "Invalid Request.<br> Try /led/1, /led/0, or /read.";
+  }
+  s += "</html>\n";
+
+  // Send the response to the client
+  client.print(s);
+  delay(1);
+  Serial.println("Client disonnected");
+
+  // The client will actually be disconnected 
+  // when the function returns and 'client' object is detroyed
 }
+}
+void setupWiFi()
+{
+  WiFi.mode(WIFI_AP);
+
+  // Do a little work to get a unique-ish name. Append the
+  // last two bytes of the MAC (HEX'd) to "Thing-":
+  uint8_t mac[WL_MAC_ADDR_LENGTH];
+  WiFi.softAPmacAddress(mac);
+  String macID = String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
+                 String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
+  macID.toUpperCase();
+  String AP_NameString = "ESP8266 Thing " + macID;
+
+  char AP_NameChar[AP_NameString.length() + 1];
+  memset(AP_NameChar, 0, AP_NameString.length() + 1);
+
+  for (int i=0; i<AP_NameString.length(); i++)
+    AP_NameChar[i] = AP_NameString.charAt(i);
+
+  WiFi.softAP(AP_NameChar, WiFiAPPSK);
+}
+
